4a266da13caad529ed540914814315a8de66f84c
vceniceros <85922025+vceniceros@users.noreply.github.com>
2023-10-31 19:59:35 -0300

    agregue los test

---
+++ ayp2_plantilla_tp_grupal_2c2023-main/tests/cmake-build-debug/gtest_lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ceni/Documentos/carpincho_TP1/ayp2_plantilla_tp_grupal_2c2023-main/tests/gtest_lib/googletest
+# Build directory: /home/ceni/Documentos/carpincho_TP1/ayp2_plantilla_tp_grupal_2c2023-main/tests/cmake-build-debug/gtest_lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
